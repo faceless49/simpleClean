de4e2a216ff8b1db64a8a5d76ecc05aa89ec5773
Egor K <digital.zeus@mail.ru>
2021-02-11 21:30:09 +0300

    support 11.02

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,7 +4,8 @@ window.addEventListener("DOMContentLoaded", () => {
   const menu = document.querySelector(".navigation__nav"),
     menuItem = document.querySelectorAll(".nav-link"),
     hamburger = document.querySelector(".header__hamburger"),
-    lines = document.querySelectorAll(".header__hamburger-item");
+    lines = document.querySelectorAll(".header__hamburger-item")
+    loginBtn = document.querySelector(".navigation__btn");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("header__hamburger--active");
@@ -23,16 +24,38 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
-});
+
+
+    loginBtn.addEventListener("click", () => {
+      hamburger.classList.toggle("header__hamburger--active");
+      menu.classList.toggle("navigation__nav--active");
+      lines.forEach((lines) => {
+        lines.classList.toggle("header__hamburger--active-item");
+      });
+    });
+  });
+
+
+// * ====== Fade click
+/* 
+window.addEventListener('click', function(event)) {
+  if (event.target === 
+} */
+
+
+
+
+
 $(document).ready(function(){
   $('.quotes__carousel-inner').slick({
     infinite: true,
     dots: true,
     speed: 1200,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 2500,
-    adaptiveHeight: true,
+    adaptiveHeight: false,
     arrows: false,
     dotsClass: 'quotes__dots'
   });
 });
+
